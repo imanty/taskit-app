5b27af6c87ef3146b686c7db8df89130f7462749
Anthony <anthonyboschman@gmail.com>
2026-04-30 18:59:29 +0800

    retrying all updates..

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -3,7 +3,7 @@
 const taskForm = document.getElementById('taskForm');
 const toDoList = document.getElementById('toDoList');
 const completedList = document.getElementById('completedList');
-const url = "http://localhost:3000";
+const url = "https://taskit-app.onrender.com";
 
 
 
