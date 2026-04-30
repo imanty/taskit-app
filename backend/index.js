353635bf9bf4099429ee34130d78c35e13fd770b
Anthony <anthonyboschman@gmail.com>
2026-04-30 19:03:00 +0800

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,16 @@ const app = express();
 
 // Middleware setup
 app.use(express.json()); // for parsing application/json
-app.use(cors("*"));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || origin.endsWith(".vercel.app")) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 // Mongo setup
 (async () => {
